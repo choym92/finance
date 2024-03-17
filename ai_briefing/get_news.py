89aad7e27dce4f8dcf14769e1ddc5108a764a46b
Paul Cho <choym92@gmail.com>
2024-03-17 11:43:12 -0400

    refactor getting news from the api

--- ai_briefing/get_news.py
+++ ai_briefing/get_news.py
@@ -15,12 +15,11 @@ def see_available_categories(newsapi):
         print("The 'categories' constant could not be found. Please refer to the News API documentation.")
 
 
-# /v2/top-headlines/sources
-
-
 # Constants
 TODAY = datetime.today().strftime("%Y-%m-%d")
 YESTERDAY = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+Q = "finance OR economy OR stock market OR cryptocurrency'"
+DOMAINS = 'marketwatch.com, cnbc.com, bloomberg.com, wsj.com, reuters.com, foxbusiness.com, bbc.com, nytimes.com, finance.yahoo.com, investing.com'
 
 # Init
 newsapi = NewsApiClient(api_key=NEWS_API_KEY)
@@ -31,43 +30,11 @@ sources = [source['id'] for source in sources_list]
 
 see_available_categories(newsapi)
 
-
-
-
 # /v2/everything
-all_articles2 = newsapi.get_everything(
-                                      q='finance, economy, stock, market, investing',
-                                      # sources='the-wall-street-journal',
-                                      from_param=YESTERDAY,  # Adjust the start date as needed
-                                      to=TODAY,  # Adjust the end date as needed
-                                      language='en',
-                                      sort_by='popularity',
-                                      # page=1,  # Adjust to explore different pages of results
-                                      # page_size=10)  # Fetch 10 articles
-)
-
-# /v2/everything
-all_articles3 = newsapi.get_everything(
-                                      # q='finance',
-                                      domains='bloomberg',
-                                      from_param=YESTERDAY,  # Adjust the start date as needed
-                                      to=TODAY,  # Adjust the end date as needed
-                                      # language='en',
-                                      sort_by='popularity',
-                                      # page=1,  # Adjust to explore different pages of results
-                                      # page_size=10)  # Fetch 10 articles
-)
-print(all_articles2)
-
-
-DOMAINS = 'marketwatch.com'
-
-Q = 'finance, market,technology, stock, investing'
-# /v2/everything
-wsj_articles = newsapi.get_everything(
-                                      q='investing',
+news_articles = newsapi.get_everything(
+                                      q=Q,
                                       # sources='bloomberg',
-                                      domains='marketwatch.com, cnbc.com, bloomberg.com, wsj.com, reuters.com, foxbusiness.com, bbc.com, nytimes.com, finance.yahoo.com, investing.com',
+                                      domains=DOMAINS,
                                       from_param=YESTERDAY,  # Adjust the start date as needed
                                       to=TODAY,  # Adjust the end date as needed
                                       language='en',
@@ -76,36 +43,14 @@ wsj_articles = newsapi.get_everything(
                                       # page_size=10)  # Fetch 10 articles
 )
 
-len(wsj_articles['articles'])
+len(news_articles['articles'])
 
-# Concatenate all article contents into one large document
-all_titles = "\n".join([article["title"] for article in all_articles2['articles']])
-all_descs = "\n".join([article["description"] for article in all_articles2['articles']])
-all_content = "\n".join([article["content"] for article in all_articles2['articles']])
+# Filter by titles, descriptions, published dates, contents
+filtered_data = [{'title': item['title'],
+                  'description': item['description'],
+                  'publishedAt': item['publishedAt'],
+                  'content': item['content']}
+                 for item in news_articles['articles']]
 
-# Assuming you want to print the titles, sources, and URLs of the articles
-for article in all_articles['articles']:
-    print(f"Title: {article['title']}")
-    print(f"Source: {article['source']['name']}")
-    print(f"URL: {article['url']}\n")
-
-# Replace 'YOUR_API_KEY' with your actual NewsAPI key
-api_key = 'YOUR_API_KEY'
-base_url = 'https://newsapi.org/v2/everything?'
-
-# Adjusting parameters to include a wider range of credible sources
-parameters = {
-    'q': 'finance AND (economy OR "stock market" OR cryptocurrency)',
-    'sources': 'bloomberg,reuters,cnbc,financial-times,the-wall-street-journal',
-    'sortBy': 'popularity',
-    'apiKey': api_key
-}
-
-response = requests.get(base_url, params=parameters)
-articles = response.json()
-
-# Print titles and URLs of top articles
-for article in articles['articles'][:5]:
-    print(f"Title: {article['title']}\nURL: {article['url']}\n")
 
 
